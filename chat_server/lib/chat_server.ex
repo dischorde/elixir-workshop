e280e075dfb52affc3fcd6c2d0078d2b58651fca
Katarina Rossi <katarina.n.rossi@gmail.com>
2017-03-25 15:47:44 -0700

    Set up GenServer

--- chat_server/lib/chat_server.ex
+++ chat_server/lib/chat_server.ex
@@ -12,12 +12,50 @@ defmodule ChatServer do
       :world
 
   """
+  use GenServer
+
   defmodule Message do
     defstruct content: "", username: "anon"
   end
-  
-  def hello do
-    :world
+
+  def start_link do
+    GenServer.start_link(__MODULE__, :ok, name: :chat_room)
+  end
+
+  def get do
+    GenServer.call(:chat_room, {:get})
+  end
+
+  def create(content) do
+    GenServer.cast(:chat_room, {:create, content})
+  end
+
+  def init(:ok) do
+    {:ok, []}
+  end
+
+  def handle_call({:create, msg}, from, state) do
+    if is_map(msg) do
+      new_state = state ++ [struct(Message, msg)]
+      {:reply, new_state, new_state}
+    else
+      new_state = state ++ [%Message{content: msg}]
+      {:reply, new_state, new_state}
+    end
+  end
+
+  def handle_call({:get}, from, state) do
+    {:reply, state, state}
+  end
+
+  def handle_cast({:create, msg}, state) do
+    if is_map(msg) do
+      new_state = state ++ [struct(Message, msg)]
+      {:noreply, new_state}
+    else
+      new_state = state ++ [%Message{content: msg}]
+      {:noreply, new_state}
+    end
   end
 
   def loop do
